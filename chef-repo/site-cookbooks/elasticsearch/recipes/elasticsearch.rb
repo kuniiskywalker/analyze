1891c82916cced0dbb60b641d66b66bc80f22079
kunii <kunii@cueplus.com>
2015-09-03 18:23:34 +0900

    [update]　elasticsearch 1.7にアップデート

--- chef-repo/site-cookbooks/elasticsearch/recipes/elasticsearch.rb
+++ chef-repo/site-cookbooks/elasticsearch/recipes/elasticsearch.rb
@@ -3,53 +3,19 @@ package "java-1.7.0-openjdk" do
   action :install
 end
 
-package "wget"
-
-## install
-bash "elasticsearch" do
-  not_if "ls /usr/local/share/elasticsearch-1.2.1"
-  user "root"
-  cwd "/usr/local/src"
-  code <<-EOH
-    wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.2.1.tar.gz
-    tar xvzf elasticsearch-1.2.1.tar.gz -C /usr/local/share/
-  EOH
+cookbook_file "/etc/yum.repos.d/elasticsearch.repo" do
+  source "elasticsearch.repo"
+  mode 00644
 end
 
-## add service
-bash "elasticsearch_as_service" do
-  not_if "ls usr/local/share/elasticsearch-1.2.1/bin/service"
-  user "root"
-  cwd "/usr/local/src"
-  code <<-EOH
-    wget https://github.com/elasticsearch/elasticsearch-servicewrapper/archive/master.zip
-    unzip /usr/local/src/master
-    mv /usr/local/src/elasticsearch-servicewrapper-master/service/ /usr/local/share/elasticsearch-1.2.1/bin/
-    /usr/local/share/elasticsearch-1.2.1/bin/service/elasticsearch install
-  EOH
+package "elasticsearch" do
+  action :install
+  options "--enablerepo=elasticsearch-1.7"
 end
-
-## elasticsearch log dir
-directory '/usr/local/share/elasticsearch-1.2.1/logs/' do
-  owner 'root'
-  group 'root'
-  mode '0755'
-  action :create
-end
-
-## elasticsearch.yml
-template "/usr/local/share/elasticsearch-1.2.1/config/elasticsearch.yml" do
-  source "elasticsearch.yml.erb"
-  owner "root"
-  group "root"
-  mode 755
-end
-
 
 ## service start
 service "elasticsearch" do
   action [ :enable, :start ]
-  subscribes :restart, resources(:template => "/usr/local/share/elasticsearch-1.2.1/config/elasticsearch.yml")
 end
 
 ## chkconfig
@@ -58,3 +24,58 @@ execute "add_elasticsearch_service" do
   command  "chkconfig elasticsearch on"
   action :run
 end
+
+
+# ## install
+# bash "elasticsearch" do
+#   not_if "ls /usr/local/share/elasticsearch-1.2.1"
+#   user "root"
+#   cwd "/usr/local/src"
+#   code <<-EOH
+#     wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.2.1.tar.gz
+#     tar xvzf elasticsearch-1.2.1.tar.gz -C /usr/local/share/
+#   EOH
+# end
+#
+# ## add service
+# bash "elasticsearch_as_service" do
+#   not_if "ls usr/local/share/elasticsearch-1.2.1/bin/service"
+#   user "root"
+#   cwd "/usr/local/src"
+#   code <<-EOH
+#     wget https://github.com/elasticsearch/elasticsearch-servicewrapper/archive/master.zip
+#     unzip /usr/local/src/master
+#     mv /usr/local/src/elasticsearch-servicewrapper-master/service/ /usr/local/share/elasticsearch-1.2.1/bin/
+#     /usr/local/share/elasticsearch-1.2.1/bin/service/elasticsearch install
+#   EOH
+# end
+#
+# ## elasticsearch log dir
+# directory '/usr/local/share/elasticsearch-1.2.1/logs/' do
+#   owner 'root'
+#   group 'root'
+#   mode '0755'
+#   action :create
+# end
+#
+# ## elasticsearch.yml
+# template "/usr/local/share/elasticsearch-1.2.1/config/elasticsearch.yml" do
+#   source "elasticsearch.yml.erb"
+#   owner "root"
+#   group "root"
+#   mode 755
+# end
+#
+#
+# ## service start
+# service "elasticsearch" do
+#   action [ :enable, :start ]
+#   subscribes :restart, resources(:template => "/usr/local/share/elasticsearch-1.2.1/config/elasticsearch.yml")
+# end
+#
+# ## chkconfig
+# execute "add_elasticsearch_service" do
+#   user "root"
+#   command  "chkconfig elasticsearch on"
+#   action :run
+# end
