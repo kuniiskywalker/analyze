e89be8b24dc0d20f347f8349a26fffe9db1a60f5
kunii <kunii@cueplus.com>
2015-09-03 18:24:03 +0900

    [add] テストデータインサートスクリプト追加

---
+++ insert_test_data.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+curl -XPUT http://localhost:9200/shakespeare -d '
+{
+ "mappings" : {
+  "_default_" : {
+   "properties" : {
+    "speaker" : {"type": "string", "index" : "not_analyzed" },
+    "play_name" : {"type": "string", "index" : "not_analyzed" },
+    "line_id" : { "type" : "integer" },
+    "speech_number" : { "type" : "integer" }
+   }
+  }
+ }
+}';
+
+wget https://www.elastic.co/guide/en/kibana/3.0/snippets/shakespeare.json
+
+curl -XPUT localhost:9200/_bulk --data-binary @shakespeare.json
+
